aff658b33c74fa59b11a53b9190172289f7764ce
Dalibor Kramar <dalik.kramar@gmail.com>
2024-01-28 14:07:10 +0100

    Find deadline

--- bot.py
+++ bot.py
@@ -4,6 +4,8 @@ import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 import time
+import requests
+import datetime
 
 banTime = 300
 
@@ -14,11 +16,10 @@ intents = discord.Intents.all()
 client = discord.Client(intents=intents)
 
 
-
 class Bot(commands.Bot):
     def __init__(self, command_prefix):
         intents = discord.Intents.all()
-        commands.Bot.__init__(self, command_prefix=command_prefix, self_bot=False, intents=intents)
+        commands.Bot.__init__(self, command_prefix=command_prefix, setlf_bot=False, intents=intents)
         self.remove_command("help")
         self.load_commands()
         self.help_message = "TODO"
@@ -31,6 +32,25 @@ class Bot(commands.Bot):
         @self.command(name="help")
         async def _help(ctx):
             await ctx.channel.send(content=self.help_message)
+
+        @self.command(name="deadline")
+        async def deadline(ctx):
+            ENDPOINT = '/meta/'
+            for year in range(23, 33):
+                for series in range(1, 5):
+                    for task in (1, 5):
+                        id = str(year) + '0' + str(series) + '0' + str(task)
+                        resp = requests.get("https://api.zisk-go.com/tasks/meta?id=" + id).json()
+                        if resp['error']:
+                            await ctx.channel.send("Request on zisk server went wrong")
+                            if ctx.message.author.has_role('Organizátor'):
+                                ctx.channel.send(resp['error'])
+                            return
+                        deadline = datetime.datetime.fromisoformat(resp['deadline'])
+                        if datetime.datetime.now() < deadline:
+                            ctx.channel.send("Nejbližší deadline je: " + str(deadline.day) + ". " + str(deadline.month) + ". " + str(deadline.year) + " v " + str(deadline.hour) + ":" + str(deadline.minute) + ".")
+                            return
+
 
         @self.command(name="forward")
         @commands.has_role('Organizátor')
